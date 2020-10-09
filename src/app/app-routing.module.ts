c92cc2bc4a9c932ffbc43d48fa1e333a69a598d3
Harjote Singh <hsing106@uottawa.ca>
2020-10-08 21:21:30 -0400

    Lab 3 base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookComponent } from './books/books.component';
 
-const routes: Routes = [];
+const booksReturn: Routes = [
+  { path: 'id', component: BookComponent }
+];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'books', component: BooksComponent, children: booksRoutes },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Routes } from "@angular/router";
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  submit(value: string) {
+    this.router.navigate(['./', value], { relativeTo: this.route });
+  }
+}
